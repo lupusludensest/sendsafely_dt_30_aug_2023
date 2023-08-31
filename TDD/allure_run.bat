4dd6519b57c697312940794e39f793c73b098f1d
Vic Gurov <gurovvic@gmail.com>
2023-08-31 12:23:58 -0500

    dt 31 aug 2023

---
+++ TDD/allure_run.bat
@@ -0,0 +1,3 @@
+pytest --alluredir=allure_tdd_reports
+pytest --alluredir=allure_tdd_reports test_sendsafely_pytested_blue_top_1.py
+pytest --alluredir=allure_tdd_reports test_sendsafely_pytested_bttns_clckd_2.py
